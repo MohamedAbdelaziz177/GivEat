18575f8d06a69c67733f6e0f56e81b4f845096e4
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-08-29 17:35:38 +0300

    solving deployment bugs IIIIIIII

--- src/main/java/com/abdelaziz26/GivEat/Configs/CorsConfig.java
+++ src/main/java/com/abdelaziz26/GivEat/Configs/CorsConfig.java
@@ -8,19 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer(CorsRegistry registry)
-    {
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                        .allowedHeaders("*");
-            }
-
-        };
-    }
+    //@Bean
+    //public WebMvcConfigurer corsConfigurer(CorsRegistry registry)
+    //{
+    //    return new WebMvcConfigurer() {
+//
+    //        @Override
+    //        public void addCorsMappings(CorsRegistry registry) {
+    //            registry.addMapping("/**")
+    //                    .allowedOrigins("*")
+    //                    .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+    //                    .allowedHeaders("*");
+    //        }
+//
+    //    };
+    //}
 }
